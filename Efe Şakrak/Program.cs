18e1f05905489076ab353e33d75830ad8439b6da
efeta <efeta@EFE-ASUS>
2022-12-11 13:30:41 +0300

    Add project files.

---
+++ Efe Şakrak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Efe_Şakrak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi1 = 58;
+            int sayi2 = 93;
+
+            int sonuc = sayi1 + sayi2;
+
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+
+        
+        
+        
+        }
+    }
+}
